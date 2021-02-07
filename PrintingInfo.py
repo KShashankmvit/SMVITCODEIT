3285277a09b56484717bc57ce1e843bb6a04036b
KShashankmvit <78546235+KShashankmvit@users.noreply.github.com>
2021-02-07 20:46:08 +0530

    Update PrintingInfo.py

--- PrintingInfo.py
+++ PrintingInfo.py
@@ -1,7 +1,7 @@
 F_name=input("Enter your first name:") 
 F_name1=F_name[0].upper() 
 L_name=input("Enter your surname:")  
-L_name1=L_name1[0].upper() 
+L_name1=L_name[0].upper() 
 age=int(input("Enter your age:")) 
 sex=input("Enter your gender(Female/Male):") 
 print(your name is:",F_name1,L_name,"your age is", 
