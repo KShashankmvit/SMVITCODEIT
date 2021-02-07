ed5c0b676d8af32fa1296a93931670c100210243
KShashankmvit <78546235+KShashankmvit@users.noreply.github.com>
2021-02-07 17:26:12 +0530

    Create Bodmas.py

---
+++ Bodmas.py
@@ -0,0 +1,8 @@
+num1=int(input("enter first number:")) 
+num2=int(input("enter second number:")) 
+num3=int(input("enter third number:")) 
+result=(num1 + num2)*num3 
+if result>100:
+   print("greater than 100") 
+else:
+   print("less than 100") 
