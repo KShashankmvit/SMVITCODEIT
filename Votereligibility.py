88c7606b60a6056ae848ea6e5868ba85ae652879
KShashankmvit <ksp220779@gmail.com>
2021-02-09 18:35:09 +0530

    simple changes

--- Votereligibility.py
+++ Votereligibility.py
@@ -2,4 +2,4 @@ age=int(input("Enter your age:"))
 if age>=18:
    print("you are eligible to vote") 
 else:
-   print("you are not eligible to vote") 
+   print("you are not eligible to vote ,to vote you have to turn 18") 
