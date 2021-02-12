57d5c36d537efe7fe719dda5dd7d5d4365254895
KShashankmvit <ksp220779@gmail.com>
2021-02-12 22:15:21 +0530

    w

---
+++ car.py
@@ -0,0 +1,10 @@
+function=""
+while function!=stop:
+    function=int(input('>'))
+    if function=="start":
+        print("car has started moving")
+    elif function=="help":
+        print("to start the vehlicle press start\n to stop vehicle press stop ") 
+    elif function=="stop":
+        print("Vehicle stops") 
+        break;                 
